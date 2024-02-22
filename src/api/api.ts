09db2f962da271266a11ff44863b8d5d5697e49e
aftab.shariati <aftab.shariati@gmail.com>
2024-02-23 01:19:11 +0330

    set Api and api type

---
+++ src/api/api.ts
@@ -0,0 +1,25 @@
+import {WeatherResponse} from "@/types/api/WeatherResponse";
+import {ForecastResponse} from "@/types/api/ForecastResponse";
+
+const baseUrl = 'https://api.openweathermap.org/data/2.5/';
+const apiKey = '3dce9b1c66837262a25b3f448d354a76';
+
+interface WeatherProps{
+    city :string
+}
+
+interface ForecastProps{
+    lat :string,
+    lon :string
+}
+
+
+export async function callWeatherApi({city} :WeatherProps) :Promise<WeatherResponse>{
+    const response = await fetch(baseUrl + `weather?q=${city}&appid=${apiKey}&units=metric`)
+    return await response.json();
+}
+
+export async function callForecastApi({lat , lon} :ForecastProps) :Promise<ForecastResponse>{
+    const response = await fetch(baseUrl + `onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`)
+    return await response.json();
+}
