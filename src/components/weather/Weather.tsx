69b5473731bb649eb9682db97cef09f4ed2ffe21
aftab.shariati <aftab.shariati@gmail.com>
2024-02-23 01:40:16 +0330

    get data from api and set

--- src/components/weather/Weather.tsx
+++ src/components/weather/Weather.tsx
@@ -2,6 +2,7 @@ import SearchForm from "@/components/weather/SearchForm";
 import WeatherInfo from "@/components/weather/WeatherInfo";
 import ForecastList from "@/components/weather/ForecastList";
 import {useState} from "react";
+import {callWeatherApi} from "@/api/api";
 interface props{
     city:string
 }
@@ -9,7 +10,7 @@ function Weather({city}:props) {
 
     const [weatherState, setWeatherState] = useState<Weather>(
         {
-            city: city,
+            city: '',
             wind:'',
             humidity:'',
             description:'',
@@ -17,8 +18,27 @@ function Weather({city}:props) {
             daily:[]
         }
     );
-    console.log(weatherState)
-    
+
+    const getWeatherData = async (city :string) =>{
+      const response =   await callWeatherApi({city})
+        console.log(response)
+
+        const weather :Weather = {
+          city : response.name,
+          wind : response.wind.speed,
+          humidity : response.main.humidity,
+          description : response.weather[0].description,
+          icon : response.weather[0].icon,
+          daily:[]
+      }
+        setWeatherState(weather);
+    }
+
+
+    if(weatherState.city.length === 0){
+        getWeatherData(city)
+    }
+
     return (
         <div>
             <SearchForm/>
